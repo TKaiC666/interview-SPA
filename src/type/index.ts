1c8d4ef9bef5c392d7881a1fff31cc54884bb6b4
TKaiC666 <tkaic666@gmail.com>
2025-05-11 13:25:54 +0800

    feature: add type file of data source

---
+++ src/type/index.ts
@@ -0,0 +1,28 @@
+// Company 和 Address 在目前需求沒用到，故先用 unknown 代替
+type Company = unknown;
+type Address = unknown;
+
+export type User = {
+  id: number;
+  name: string;
+  userName: string;
+  phone: string;
+  website: string;
+  company: Company;
+  address: Address;
+};
+
+export type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+export type Comment = {
+  id: number;
+  postId: number;
+  name: string;
+  email: string;
+  body: string;
+};
