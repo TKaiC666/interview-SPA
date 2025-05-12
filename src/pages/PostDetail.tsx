bc1d2e4cb6b16fbd99220adde6800dd8435adc16
TKaiC666 <tkaic666@gmail.com>
2025-05-13 00:12:00 +0800

    feat: implement PostDetail

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,8 +1,40 @@
 import { useParams } from "react-router";
+import usePostDetail from "@/hooks/usePostDetail";
 
 const PostDetail = () => {
-  const { postId } = useParams();
-  return <>{postId}</>;
+  const postId = Number(useParams().postId);
+  const { data, isLoading, error } = usePostDetail(postId);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      {data && (
+        <>
+          <ul>
+            <li>{data.id}</li>
+            <li>{data.username}</li>
+            <li>{data.title}</li>
+            <li>{data.body}</li>
+          </ul>
+          <ul>
+            {data.comments.map((comment) => (
+              <li key={comment.id}>
+                <span>{comment.name}</span>
+                <span>{comment.body}</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default PostDetail;
