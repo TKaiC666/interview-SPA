e0be3f32c8dd8827fb740dae5d909c3da171ef68
TKaiC666 <tkaic666@gmail.com>
2025-05-13 00:19:10 +0800

    feat: show page not found when user access illegal route

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Posts from "@/pages/Posts";
 import PostDetail from "@/pages/PostDetail";
+import PageNotFound from "@/pages/pageNotFound";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Posts />} />
+      <Route path="/post-detail" element={<PageNotFound />} />
       <Route path="/post-detail/:postId" element={<PostDetail />} />
     </Routes>
   </BrowserRouter>
