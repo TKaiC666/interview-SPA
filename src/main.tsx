4b362eb18d9ac1d0ed044a286b491396fd79610f
TKaiC666 <tkaic666@gmail.com>
2025-05-12 21:47:28 +0800

    modi: remove StrictMode to avoid twice rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Posts from "@/pages/Posts";
 import PostDetail from "@/pages/PostDetail";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Posts />} />
-        <Route path="/post-detail/:postId" element={<PostDetail />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Posts />} />
+      <Route path="/post-detail/:postId" element={<PostDetail />} />
+    </Routes>
+  </BrowserRouter>
 );
