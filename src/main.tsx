123c902c99d9932d29c1726965bd2bf0f848196b
TKaiC666 <tkaic666@gmail.com>
2025-05-13 23:19:07 +0700

    feat: update route to show page not found

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Posts from "@/pages/Posts";
 import PostDetail from "@/pages/PostDetail";
-import PageNotFound from "@/pages/pageNotFound";
+import PageNotFound from "@/pages/PageNotFound";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Posts />} />
-      <Route path="/post-detail" element={<PageNotFound />} />
       <Route path="/post-detail/:postId" element={<PostDetail />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   </BrowserRouter>
 );
